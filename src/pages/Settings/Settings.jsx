1d74817ac4c68de839bd17f515634e9597db6c6a
AYANO_EMMANUEL <ayanoifeoluwa567@gmail.com>
2024-12-18 18:51:43 +0100

    added the other part of the settings(center)

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import TopRightMenu from "../../components/TopRightMenu";
 import RightNav from "../../components/RightNav";
-import { AiOutlineCamera, AiOutlineCopy } from "react-icons/ai";
+import { AiOutlineCamera, AiOutlineCopy, AiOutlineLock } from "react-icons/ai";
+import { FiKey } from "react-icons/fi";
+import { RiInboxUnarchiveLine } from "react-icons/ri";
+import { BsQuestionDiamondFill } from "react-icons/bs";
+import { MdSecurity, MdTextsms } from "react-icons/md";
+import { GrPower } from "react-icons/gr";
+import { CgInfo } from "react-icons/cg";
 
 const Settings = () => {
     return (
@@ -12,12 +18,88 @@ const Settings = () => {
             <div className="text-5xl">Settings</div>
             <TopRightMenu styles="block lg:hidden" />
           </div>
-          <div className="flex space-x-6 my-6">
-            
+          <div className="flex flex-col my-6">
+            <div className="bg-slate-50 rounded-2xl p-3 flex flex-col my-3 gap-y-6">
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <FiKey height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">Login Settings</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2"> {">"} </span>
+              </div>
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <AiOutlineLock height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">Payment Settings</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2"> {">"} </span>
+              </div>
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <RiInboxUnarchiveLine height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">Saving Settings</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2"> {">"} </span>
+              </div>
+            </div>
+            <div className="bg-slate-50 rounded-2xl p-3 flex flex-col my-3 gap-y-6">
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <BsQuestionDiamondFill height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">Security Questions</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2"> {">"} </span>
+              </div>
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <MdTextsms height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">SMS Alert Subscription</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2"> {">"} </span>
+              </div>
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <GrPower height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">Close Account</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2"> {">"} </span>
+              </div>
+            </div>
+            <div className="bg-slate-50 rounded-2xl p-3 flex flex-col my-3 gap-y-6">
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <MdSecurity height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">Security Center</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2"> {">"} </span>
+              </div>
+            </div>
+            <div className="bg-slate-50 rounded-2xl p-3 flex flex-col my-3 gap-y-6">
+              <div className="flex justify-between align-middle my-auto py-auto">
+                <div className="flex justify-between align-middle my-auto py-auto">
+                  {/* <span className="bg-slate-100 rounded-xl p-1 px-2">Tier 1</span> */}
+                  <CgInfo height={'100%'} size={'30'} className="ms-2 h-full" />
+                  <span className="opacity-70 pl-4 text-xl">About</span>
+                </div>
+                <span className="opacity-45 rounded-xl ml-2 p-1 px-2">Version 1.0  {">"} </span>
+              </div>
+            </div>
+            <div className="bg-slate-50 rounded-2xl p-3 flex flex-col my-3 gap-y-6">
+              <div className="flex justify-center text-2xl align-middle my-auto py-auto">
+                Sign Out
+              </div>
+            </div>
           </div>
         </div>
         <div className="border-l-[1px] border-gray-100 h-screen flex-[1.6] lg:flex-[1.1] hidden lg:block">
-          <TopRightMenu styles={'pt-12 pr-6'} class="hidden lg:visible"/>
+          <TopRightMenu styles={'pt-10 pr-6'} class="hidden lg:visible"/>
           <div className="">
             <div className="w-max bg-slate-50 p-8 m-auto cursor-pointer hover:bg-slate-200 hover:opacity-25 rounded-full my-4">
               <AiOutlineCamera size={'30px'} />
@@ -53,7 +135,7 @@ const Settings = () => {
                 <span className="opacity-70">Core Account No:</span>
                 <div className="flex align-middle">
                   <span className="">+234 8162638034 </span>
-                  <AiOutlineCopy className="text-green-400 ml-1" />
+                  <AiOutlineCopy className="text-green-400 cursor-pointer ml-1" />
                 </div>
               </div>
               <div className="flex justify-between">
