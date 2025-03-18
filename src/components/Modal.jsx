6614f78289db3c7d27b7f3dff9c5885d9ed36c21
AYANO_EMMANUEL <ayanoifeoluwa567@gmail.com>
2025-03-18 14:41:36 +0100

    added to the modal on the dashboard

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,29 +21,4 @@ const Modal = ({ children }) => {
   );
 };
 
-// const Modal = ({ isOpen, onClose, children }) => {
-//   if (!isOpen) return null;
-
-//   return (
-//     <div className="modal-backdrop flex align-middle justify-center z-[1000] bg-slate-400 fixed top-0 left-0 w-full " onClick={onClose}>
-//       <div 
-//         className="modal-content bg-white relative w-[90%] max-w-[600px] p-6 rounded-lg shadow-lg animate-slideIn"
-//         onClick={(e) => e.stopPropagation()}
-//         role="dialog"
-//         aria-modal="true"
-//         style={{ animation: 'slideIn 0.3s ease-out' }}
-//       >
-//         {children}
-//         <button 
-//           className="modal-close absolute top-[1rem] right-[1rem] text-gray-500 hover:text-gray-700 size-[1.5rem] cursor-pointer border-none"
-//           onClick={onClose}
-//           aria-label="Close modal"
-//         >
-//           &times;
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
 export default Modal;
